6202bb00599426f9325fdc86b033f29966d6b93e
karan singh D <ceitkaransingh2026@gmail.com>
2024-04-04 22:49:56 +0530

    added usecallback

---
+++ usecallbacks/src/App.jsx
@@ -0,0 +1,36 @@
+import { useCallback, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Child from './components/Child'
+
+
+function App() {
+  const [add, setadd] = useState(0)
+  const [count, setcount] = useState(0)
+
+    // const  learning =()=>{
+    //     // helo inside a the func
+    // }
+
+    const learning= useCallback(()=>{
+      // using use call back
+    },[count])
+
+
+  return (
+    <>
+      <div>
+        <h1>learning usecallback</h1>
+        <Child learning={learning} count={count}/>
+        <h1>{add}</h1>
+        <h1>{count}</h1>
+        <button onClick={()=>setadd(add+1)}>addition</button>
+        <button onClick={()=>setcount(count+2)}>count</button>
+      </div> 
+    </>
+  )
+}
+
+
+export default App
---
+++ usecallbacks/src/components/Child.jsx
@@ -0,0 +1,12 @@
+import React, { memo } from 'react'
+
+const Child = ({learning,count}) => {
+  console.log("child hu");
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default memo(Child);
